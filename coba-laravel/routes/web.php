0b5f8706e839deb66cba104e00abdf46a8d9932a
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-31 08:24:02 +0700

    video3laravel

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Hafizh Faiz Fadhlurrahman",
+        "email" => "hafizhfaiz11@gmail.com",
+        "image" => "Hafizh.png"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');  
 });
