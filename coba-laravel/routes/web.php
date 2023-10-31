41ed7114b4e248407bbc276db484121c9ac9e28e
hafizfaiz <hafizhfaiz11@gmail.com>
2023-10-31 09:37:55 +0700

    video4laravel

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,15 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "Home"
+    ]);
 });
 Route::get('/about', function () {
     return view('about',[
+        "title" => "About",
         "name" => "Hafizh Faiz Fadhlurrahman",
         "email" => "hafizhfaiz11@gmail.com",
         "image" => "Hafizh.png"
     ]);
 });
+
+
+
 Route::get('/blog', function () {
-    return view('posts');  
+    $blog_posts = [[
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Hafizh Faiz",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae possimus laborum temporibus eum veritatis accusantium nobis, ipsa facilis fuga ducimus similique voluptate aut illum ipsum tempore quisquam, ut rem amet repellat beatae inventore quibusdam. Eum rem quia aut eos laborum deserunt tempore commodi fugiat maxime et totam itaque, sed, quibusdam accusantium, dignissimos adipisci odit vitae sint eaque! Vel modi facilis suscipit non veniam molestias eum adipisci, fuga dolorum quasi expedita culpa iste officia voluptates hic mollitia incidunt? Error, illo!"
+    
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Hafizh Faiz",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae possimus laborum temporibus eum veritatis accusantium nobis, ipsa facilis fuga ducimus similique voluptate aut illum ipsum tempore quisquam, ut rem amet repellat beatae inventore quibusdam. Eum rem quia aut eos laborum deserunt tempore commodi fugiat maxime et totam itaque, sed, quibusdam accusantium, dignissimos adipisci odit vitae sint eaque! Vel modi facilis suscipit non veniam molestias eum adipisci, fuga dolorum quasi expedita culpa iste officia voluptates hic mollitia incidunt? Error, illo!"
+    
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman  
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [[
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Hafizh Faiz",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae possimus laborum temporibus eum veritatis accusantium nobis, ipsa facilis fuga ducimus similique voluptate aut illum ipsum tempore quisquam, ut rem amet repellat beatae inventore quibusdam. Eum rem quia aut eos laborum deserunt tempore commodi fugiat maxime et totam itaque, sed, quibusdam accusantium, dignissimos adipisci odit vitae sint eaque! Vel modi facilis suscipit non veniam molestias eum adipisci, fuga dolorum quasi expedita culpa iste officia voluptates hic mollitia incidunt? Error, illo!"
+    
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Hafizh Faiz",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quae possimus laborum temporibus eum veritatis accusantium nobis, ipsa facilis fuga ducimus similique voluptate aut illum ipsum tempore quisquam, ut rem amet repellat beatae inventore quibusdam. Eum rem quia aut eos laborum deserunt tempore commodi fugiat maxime et totam itaque, sed, quibusdam accusantium, dignissimos adipisci odit vitae sint eaque! Vel modi facilis suscipit non veniam molestias eum adipisci, fuga dolorum quasi expedita culpa iste officia voluptates hic mollitia incidunt? Error, illo!"
+    
+        ]
+    ];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
