bcd18a3e48794283f9cf5644b46ba75fdea172fc
hafizfaiz <hafizhfaiz11@gmail.com>
2023-11-05 21:04:56 +0700

    video11laravel

--- coba-laravel/resources/views/category.blade.php
+++ coba-laravel/resources/views/category.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+<!-- @extends('layouts.main')
 
 @section('container')
  <h1 class="mb-5">Post Category : {{ $category }}</h1>
@@ -11,4 +11,4 @@
     <p>{{ $post->excerpt }}</p>
   </article>
   @endforeach
-@endsection
+@endsection -->
