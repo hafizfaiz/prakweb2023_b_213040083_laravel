fbf1333c6e789241b86ba2cf3f23716bc3efe42c
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-11-12 21:53:06 +0700

    video13laravel

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -16,39 +16,26 @@ use App\Models\User;
 */
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home" 
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Hafizh Faiz Fadhlurrahman",
         "email" => "hafizhfaiz11@gmail.com",
         "image" => "Hafizh.png"
     ]);
 });
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
