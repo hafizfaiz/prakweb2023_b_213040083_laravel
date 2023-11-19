722f76de7580a55caad838d73370101b5d0fde3f
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-11-19 23:38:14 +0700

    video17laravel

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -13,26 +13,22 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, function($query, $search) {
-            return $query->where(function($query) use ($search) {
-                 $query->where('title', 'like', '%' . $search . '%')
-                             ->orWhere('body', 'like', '%' . $search . '%');
-             });
-         });
+        $query->when($filters['search'] ?? false,function($query, $search){
+            return $query->where('title','like','%' .$search.'%')
+                         ->orWhere('body','like','%'.$search.'%');
+        });
 
          $query->when($filters['category']??false, function($query,$category) {
             return $query->whereHas('category', function($query) use($category){
                 $query->where('slug', $category);
             });
         }); 
-
         $query->when($filters['author']??false, fn($query, $author)=>
             $query->whereHas('author', fn($query)=>
                 $query->where('username', $author)
             )
         );
     }
-
     public function category()
     {
         return $this->belongsTo(Category::class);   
@@ -40,5 +36,8 @@ class Post extends Model
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+    public function getRouteKeyName() {
+        return 'slug';
     }
 }
