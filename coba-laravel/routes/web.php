7e3db0a9bfaf5a9dd3886ea961152c9378631d85
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-11-19 21:31:59 +0700

    video14laravel

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,9 +1,9 @@
 <?php
-use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,3 +39,9 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
