63b261ec860f9509c0d2fba2c29c3a40f0a1cb02
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-11-26 23:16:32 +0700

    video23laravel

--- coba-laravel/app/Providers/AppServiceProvider.php
+++ coba-laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+         });
     }
 }
