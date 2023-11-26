9f86efeccfe4beb4a46d71879abe339adde780cf
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-11-26 23:08:52 +0700

    video22laravel

--- coba-laravel/resources/views/dashboard/posts/create.blade.php
+++ coba-laravel/resources/views/dashboard/posts/create.blade.php
@@ -42,8 +42,9 @@
     </div>
     <div class="mb-3">
         <label for="image" class="form-label">Post Image</label>
+        <img class="img-preview img-fluid mb-3 col-sm-5">
         <input class="form-control @error('image') is-invalid @enderror" type="file" id="image"
-        name="image">
+        name="image" onchange="previewImage()">
         @error('image')
           <div class="invalid-feedback">
             {{ $message }}
@@ -72,5 +73,15 @@
   document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     });
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+        imgPreview.style.display = 'block';
+        const ofReader = new FileReader();
+        ofReader.readAsDataURL(image.files[0]);
+        ofReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 @endsection
